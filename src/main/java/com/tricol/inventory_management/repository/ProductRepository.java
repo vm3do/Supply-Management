c74e88bed4436605cf48c9c57af7a4782957aa29
vm3do <medoavvadi@gmail.com>
2025-11-03 06:51:01 +0100

    feat: create product repo using jpaRepository

---
+++ src/main/java/com/tricol/inventory_management/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.tricol.inventory_management.repository;
+
+import com.tricol.inventory_management.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long>{
+}
